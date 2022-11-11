c1175f2897a5064b9a142d6477e8d9c49baa153e
jennsommer02 <jennifersommer02@gmail.com>
2022-11-11 15:29:09 -0500

    Navigation binding components

--- app/src/main/java/com/team3/indexify/MainActivity.kt
+++ app/src/main/java/com/team3/indexify/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_history, R.id.navigation_home, R.id.navigation_about
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
---
+++ app/src/main/java/com/team3/indexify/ui/about/AboutViewModel.kt
@@ -0,0 +1,7 @@
+package com.team3.indexify.ui.about
+
+import androidx.lifecycle.ViewModel
+
+class AboutViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
