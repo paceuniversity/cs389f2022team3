bfd5f0daae6fdbfe12dd1cfdeb613f8f2785d216
jennsommer02 <jennifersommer02@gmail.com>
2022-11-14 13:12:44 -0500

    Fixes missing import

--- app/src/main/java/com/team3/indexify/network/BlueApiService.kt
+++ app/src/main/java/com/team3/indexify/network/BlueApiService.kt
@@ -2,6 +2,7 @@ package com.team3.indexify.network
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.team3.indexify.models.SensorData
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
