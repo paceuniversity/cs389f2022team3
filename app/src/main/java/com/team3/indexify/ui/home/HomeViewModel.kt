1c4c3937570d71977526be0c1e176b3108b96d75
Andrew Iadevaia <aiadevaia@pace.edu>
2022-11-30 11:21:18 -0500

    Adds MutableLiveData to HomeViewModel.kt

--- app/src/main/java/com/team3/indexify/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/team3/indexify/ui/home/HomeViewModel.kt
@@ -4,14 +4,22 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.team3.indexify.network.models.SensorDataModel
 import kotlinx.coroutines.launch
 
-enum class BlueApiStatus { LOADING, ERROR, DONE }
-
+enum class ColabApiStatus { LOADING, ERROR, DONE }
 /**
  * The [ViewModel] that is attached to the [HomeFragment].
  */
 class HomeViewModel : ViewModel() {
 
+    private val _status = MutableLiveData<ColabApiStatus>()
+    val status: LiveData<ColabApiStatus> = _status
+
+    private val _sensorData = MutableLiveData<SensorDataModel>()
+    val sensorData: LiveData<SensorDataModel> = _sensorData
+
+    private val _selectedStation = MutableLiveData<String>()
+    val selectedStation: LiveData<String> = _selectedStation
 
 }
