678bd5b2fdaa4dac97819f9c9f08728e6b64fda0
Mattbavaro32 <mb46224p@pace.edu>
2022-11-30 10:31:22 -0500

    Fixes SensorDataModel.kt

--- app/src/main/java/com/team3/indexify/network/models/SensorDataModel.kt
+++ app/src/main/java/com/team3/indexify/network/models/SensorDataModel.kt
@@ -3,17 +3,6 @@ package com.team3.indexify.network.models
 data class SensorDataModel(
     val deployment_id: Int,
     val measurement: String,
-    val sensors: Sensors,
+    val sensors: List<SensorModel>,
     val timestamp: String
-) {
-    data class Sensors(
-        val Cond: Double,
-        val DOpct: Double,
-        val Sal: Double,
-        val Temp: Double,
-        val Turb: Double,
-        val lat: Double,
-        val lon: Double,
-        val pH: Double
-    )
-}
+)
