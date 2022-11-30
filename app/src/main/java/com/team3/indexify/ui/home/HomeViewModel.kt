26480438c1bbe03c5211fd1a865ffc3d01cfcc12
Andrew Iadevaia <aiadevaia@pace.edu>
2022-11-30 11:22:08 -0500

    Adds data pull

--- app/src/main/java/com/team3/indexify/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/team3/indexify/ui/home/HomeViewModel.kt
@@ -1,9 +1,11 @@
 package com.team3.indexify.ui.home
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.team3.indexify.network.core.ColabApi
 import com.team3.indexify.network.models.SensorDataModel
 import kotlinx.coroutines.launch
 
@@ -28,5 +30,20 @@ class HomeViewModel : ViewModel() {
 
     fun resetView() {
         _selectedStation.value = "Ada"
+    }
+
+    fun refreshView() {
+        viewModelScope.launch {
+            _status.value = ColabApiStatus.LOADING
+            try {
+                Log.v("refreshView", _selectedStation.value.toString())
+                _sensorData.value = ColabApi.retrofitService.getSensorData(_selectedStation.value.toString())
+                Log.v("refreshView", _sensorData.value!!.deployment_id.toString())
+                _status.value = ColabApiStatus.DONE
+            } catch (e: Exception) {
+                _status.value = ColabApiStatus.ERROR
+                Log.v("refreshView", e.toString())
+            }
+        }
     }
 }
