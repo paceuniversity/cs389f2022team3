830774d1b130a11db7340fa1b883382c4a6f8fed
Mattbavaro32 <mb46224p@pace.edu>
2022-11-30 10:21:29 -0500

    Changes API URL to HTTP

--- app/src/main/java/com/team3/indexify/network/BlueApiService.kt
+++ app/src/main/java/com/team3/indexify/network/BlueApiService.kt
@@ -7,7 +7,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
-private const val BASE_URL = "https://api.bluecolab.cc/"
+private const val BASE_URL = "http://droid.bluecolab.cc/"
 
 /**
  * Build the Moshi object that Retrofit will be using, making sure to add the Kotlin adapter for
