7c390b3378a7d850a1e3d751fb2733d9e4198cb6
Andrew Iadevaia <aiadevaia@pace.edu>
2022-11-30 11:25:31 -0500

    Adds func to populate spinner

--- app/src/main/java/com/team3/indexify/ui/home/HomeFragment.kt
+++ app/src/main/java/com/team3/indexify/ui/home/HomeFragment.kt
@@ -6,8 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.fragment.app.viewModels
+import com.team3.indexify.R
 import com.team3.indexify.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -29,6 +33,31 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        binding.lifecycleOwner = this
+        binding.viewModel = homeViewModel
+
+        val spinner: Spinner = binding.stationSpinner
+        ArrayAdapter.createFromResource(
+            this.requireActivity(),
+            R.array.station_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+            spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    println("erreur")
+                }
+
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    val station = parent?.getItemAtPosition(position).toString()
+                    homeViewModel.setStation(station)
+                }
+
+            }
+        }
 
         return root
     }
