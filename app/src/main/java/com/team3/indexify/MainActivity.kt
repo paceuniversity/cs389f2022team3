38cd9906bddc15b18913d89f30894b2c975c1d8a
Falco-code <bowser625@gmail.com>
2022-11-30 11:16:36 -0500

    Fixes navigation in MainActivity.kt

--- app/src/main/java/com/team3/indexify/MainActivity.kt
+++ app/src/main/java/com/team3/indexify/MainActivity.kt
@@ -19,15 +19,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
         val navView: BottomNavigationView = binding.navView
 
-        val navController = navHostFragment.navController
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_history, R.id.navigation_home, R.id.navigation_about
+                R.id.navigation_home, R.id.navigation_history, R.id.navigation_about
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
