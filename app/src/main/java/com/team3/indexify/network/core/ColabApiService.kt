d5b68fdf93002bec96223cc4b411b8d4fa89d1bf
jennsommer02 <jennifersommer02@gmail.com>
2022-12-14 00:26:56 -0500

    Updates ColabApiService.kt

--- app/src/main/java/com/team3/indexify/network/core/ColabApiService.kt
+++ app/src/main/java/com/team3/indexify/network/core/ColabApiService.kt
@@ -2,6 +2,7 @@ package com.team3.indexify.network.core
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.team3.indexify.network.models.SensorDataModel
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
