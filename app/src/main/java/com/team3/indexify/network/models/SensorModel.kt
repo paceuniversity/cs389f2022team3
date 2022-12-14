cc075a4d6697af1cbcb9a133374e6d5f5de2ecfb
jennsommer02 <jennifersommer02@gmail.com>
2022-12-14 00:26:27 -0500

    Adds new SensorModel.kt

---
+++ app/src/main/java/com/team3/indexify/network/models/SensorModel.kt
@@ -0,0 +1,12 @@
+package com.team3.indexify.network.models
+
+data class SensorModel(
+    val Cond: Double,
+    val DOpct: Double,
+    val Sal: Double,
+    val Temp: Double,
+    val Turb: Double,
+    val lat: Double,
+    val lon: Double,
+    val pH: Double
+)
